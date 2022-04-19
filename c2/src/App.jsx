e0141f6293f85a9f164d869a5a77f56fe76ac9ca
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-04-19 15:09:05 +0530

    Update App.jsx

--- c2/src/App.jsx
+++ c2/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <button className="togglebtn" onClick={() => {
         setShow(!show)
-      }}>{ show?"Add Student":"Table"}</button>
+      }}>{ show?"Add A new Student":"Go to Table"}</button>
 
       {show?<ShowStudents/>:<AddStudent/>}
       {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
